84fafad82a0e370bfb7d556fb4b2544041c83e88
Satoshi Kito <kitos@kawai.co.jp>
2022-02-24 10:27:00 +0900

    まだ途中

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,13 @@ fn main() {
     let root_number = rand::thread_rng().gen_range(0, 1001);
     println!("root_number is:{}", root_number); //秘密の数字は次の通り:{}
 
-
+//ループでよき数字入れてもらう
     loop{
-
-        //まず数字Aいれてね
     println!("Please input number a:");
 
     let mut a_number = String::new();
 
-    io::stdin().read_line(&mut a_number)   //←         できてない
+    io::stdin().read_line(&mut a_number)   //← よく理解してない
         .expect("Failed to read line");
 
     let a_number: u32 = match a_number.trim().parse(){
@@ -32,4 +30,11 @@ fn main() {
 
     break;
     }
+//ここで計算したらいいかな？
+
+
+
+
 }
+
+//関数とかまだわからない
